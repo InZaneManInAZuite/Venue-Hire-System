ca7734f2c3dffe8ab41e0a85b4698d5f3beb8fee
Toshiro <toshiro.mendoza@gmail.com>
2024-03-25 13:04:52 +1300

    Initiating a transfer to class Booking and Venue

--- src/main/java/nz/ac/auckland/se281/Booking.java
+++ src/main/java/nz/ac/auckland/se281/Booking.java
@@ -1,12 +1,13 @@
 package nz.ac.auckland.se281;
 
 public class Booking {
-  private String date;
   private String ref;
-  private String code;
-  private String venue;
+  private String bookDate;
+  private String checkIn;
+  private String venueCode;
+  private String venueName;
   private String email;
   private String attendee;
 
-  
+  public Booking() {}
 }
---
+++ src/main/java/nz/ac/auckland/se281/Venue.java
@@ -0,0 +1,13 @@
+package nz.ac.auckland.se281;
+
+import java.util.ArrayList;
+
+public class Venue {
+  private String names;
+  private String venueCodes;
+  private String capacities;
+  private String hireFees;
+  private ArrayList<Booking> bookings = new ArrayList<Booking>();
+
+  public Venue() {}
+}
