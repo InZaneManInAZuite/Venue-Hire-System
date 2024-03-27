349ee1787434ff5470d6bc31096b3f2379b7d08b
Toshiro <toshiro.mendoza@gmail.com>
2024-03-27 13:55:19 +1300

    Privatized data but accessible within the venue class

--- src/main/java/nz/ac/auckland/se281/Venue.java
+++ src/main/java/nz/ac/auckland/se281/Venue.java
@@ -5,21 +5,41 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Venue {
-  public String name;
-  public String venueCode;
-  public String capacity;
-  public String hireFee;
-  public String earliest;
-  public ArrayList<Booking> bookings = new ArrayList<Booking>();
-  public int numOfVenueBookings = 0;
+  private String name;
+  private String code;
+  private String capacity;
+  private String hireFee;
+  private String earliest;
+  private ArrayList<Booking> bookings = new ArrayList<Booking>();
+  private int numOfVenueBookings = 0;
 
   public Venue() {}
 
-  public Venue(String name, String venueCode, String capacity, String hireFee) {
+  public Venue(String name, String code, String capacity, String hireFee) {
     this.name = name;
-    this.venueCode = venueCode;
+    this.code = code;
     this.capacity = capacity;
     this.hireFee = hireFee;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getCode() {
+    return this.code;
+  }
+
+  public String getCapacity() {
+    return this.capacity;
+  }
+
+  public String getHireFee() {
+    return this.hireFee;
+  }
+
+  public String getEarliest() {
+    return this.earliest;
   }
 
   public boolean isDateBooked (String date) {
@@ -31,8 +51,9 @@ public class Venue {
     return false;
   }
 
-  public void add (Booking booking) {
+  public void addBooking (Booking booking) {
     bookings.add(booking);
+    numOfVenueBookings++;
     while (isDateBooked(this.earliest)) {
       incrementEarliest();
     }
@@ -45,6 +66,11 @@ public class Venue {
               Integer.parseInt(dateSplit[1]), 
               Integer.parseInt(dateSplit[0]));
     date.add(Calendar.DAY_OF_MONTH, 1);
+
+    
+    this.earliest = date.get(Calendar.DAY_OF_MONTH) + "/" +
+                    date.get(Calendar.MONTH) + "/" +
+                    date.get(Calendar.YEAR);
   }
 
 }
